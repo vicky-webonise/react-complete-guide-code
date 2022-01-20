f89cfcbacf857fb4d71b3009e1e3e6e1d935472f
Vicky Shinde <vicky.shinde@weboniselab.com>
2022-01-20 19:40:31 +0530

    removed unwanted files

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">learn react</div>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-import './sass/global.sass';
+import './assets/sass/global.sass';
 
 ReactDOM.render(
   <React.StrictMode>
